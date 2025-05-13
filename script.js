d36cf944189685d81ee480a64d649f7d691d07bf
KateriumG <kevinjoelescobar2020@gmail.com>
2025-05-13 17:07:22 -0500

    empezamos bien

---
+++ script.js
@@ -0,0 +1,42 @@
+let carrito = [];
+let total = 0;
+
+function agregarAlCarrito(nombreProducto, precioProducto){
+    const producto = {
+        nombre: nombreProducto,
+        precio: precioProducto
+    };
+    carrito.push(producto);
+    actualizarCarrito();
+}
+
+function actualizarCarrito(){
+    const listaCarrito = document.getElementById("listaCarrito");
+    const totalPagar = document.getElementById("totalPagar");
+
+    listaCarrito.innerHTML = "";
+    total = 0;
+
+    carrito.forEach((producto) => {
+        total += producto.precio;
+        const item = document.createElement("div");
+        item.className = "item-carrito";
+        item.innerHTML = `
+        <span>${producto.nombre}</span>
+        <span>$${producto.precio}</span>
+
+        `;
+        listaCarrito.appendChild(item);
+    });
+    totalPagar.textContent = total;
+}
+
+function finalizarCompra(){
+    if (carrito.length === 0){
+        alert("El carrito esta vacio");
+        return
+    }
+    alert(`Gracias por tu compra. Total a pagar: $${total}`);
+    carrito = [];
+    actualizarCarrito();
+}
