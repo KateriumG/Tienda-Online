3a2b63f3518e8390ee5274a9b0633fac25e6e8b9
KateriumG <kevinjoelescobar2020@gmail.com>
2025-05-13 17:10:35 -0500

    rough continuation

--- script.js
+++ script.js
@@ -1,42 +1,43 @@
 let carrito = [];
 let total = 0;
 
-function agregarAlCarrito(nombreProducto, precioProducto){
-    const producto = {
-        nombre: nombreProducto,
-        precio: precioProducto
-    };
-    carrito.push(producto);
-    actualizarCarrito();
+function agregarAlCarrito(nombreProducto, precioProducto) {
+  const producto = {
+    nombre: nombreProducto,
+    precio: precioProducto
+  };
+  carrito.push(producto);
+  actualizarCarrito();
 }
 
-function actualizarCarrito(){
-    const listaCarrito = document.getElementById("listaCarrito");
-    const totalPagar = document.getElementById("totalPagar");
+function actualizarCarrito() {
+  const listaCarrito = document.getElementById('listaCarrito');
+  const totalPagar = document.getElementById('totalPagar');
 
-    listaCarrito.innerHTML = "";
-    total = 0;
+  listaCarrito.innerHTML = '';
+  total = 0;
 
-    carrito.forEach((producto) => {
-        total += producto.precio;
-        const item = document.createElement("div");
-        item.className = "item-carrito";
-        item.innerHTML = `
-        <span>${producto.nombre}</span>
-        <span>$${producto.precio}</span>
+  carrito.forEach((producto) => {
+    total += producto.precio;
+    const item = document.createElement('div');
+    item.className = 'item-carrito';
+    item.innerHTML = `
+      <span>${producto.nombre}</span>
+      <span>$${producto.precio}</span>
+    `;
+    listaCarrito.appendChild(item);
+  });
 
-        `;
-        listaCarrito.appendChild(item);
-    });
-    totalPagar.textContent = total;
+  totalPagar.textContent = total;
 }
 
-function finalizarCompra(){
-    if (carrito.length === 0){
-        alert("El carrito esta vacio");
-        return
-    }
-    alert(`Gracias por tu compra. Total a pagar: $${total}`);
-    carrito = [];
-    actualizarCarrito();
+function finalizarCompra() {
+  if (carrito.length === 0) {
+    alert('El carrito está vacío');
+    return;
+  }
+
+  alert(`Gracias por tu compra. Total a pagar: $${total}`);
+  carrito = [];
+  actualizarCarrito();
 }
