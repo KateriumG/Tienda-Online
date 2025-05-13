eccc95d14a8bc36a1c3ca5c1244cc23ecc145641
KateriumG <kevinjoelescobar2020@gmail.com>
2025-05-13 18:15:40 -0500

    Update script.js

--- script.js
+++ script.js
@@ -24,6 +24,7 @@ function actualizarCarrito() {
     item.innerHTML = `
       <span>${producto.nombre}</span>
       <span>$${producto.precio}</span>
+      <button onclick"">Borrar</button>
     `;
     listaCarrito.appendChild(item);
   });
